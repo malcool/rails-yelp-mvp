7bd3a077d9862f9ba8652e882fa1b55263c0e317
Malcolm Remple <hello@malcolmremple.com>
2019-11-13 17:05:37 -0500

    all working

--- app/controllers/restaurants_controller.rb
+++ app/controllers/restaurants_controller.rb
@@ -13,16 +13,17 @@ class RestaurantsController < ApplicationController
 
   def create
     @restaurant = Restaurant.new(restaurant_params)
-    # if @restaurant.save
-    #   redirect_to restaurants_path
-    # else
-    #   render :new
-    # end
+    if @restaurant.save
+      
+      redirect_to restaurant_path(@restaurant)
+    else
+      render :new
+    end
   end
 
   private
 
   def restaurant_params
-    params.require(:restaurant).permit(:name, :address, :description, :stars)
+    params.require(:restaurant).permit(:name, :address, :phone_number, :category)
   end
 end
