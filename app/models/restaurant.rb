bb135882896417d17e538e6bb49e73f8fcf050e2
Malcolm Remple <hello@malcolmremple.com>
2019-11-13 12:06:53 -0500

    done with validations

--- app/models/restaurant.rb
+++ app/models/restaurant.rb
@@ -1,3 +1,8 @@
 class Restaurant < ApplicationRecord
-  belongs_to :review
+  has_many :reviews, dependent: :destroy
+
+  validates :name, presence: true
+  validates :address, presence: true
+  validates :category, presence: true
+  validates :category, inclusion: { in: ['chinese', 'italian', 'japanese', 'french', 'belgian'] }
 end
