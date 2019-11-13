f3548984e46ba229ad82ae77fd9479c56aa5a1e0
Malcolm Remple <hello@malcolmremple.com>
2019-11-13 14:54:03 -0500

    comtrollers are up

--- config/routes.rb
+++ config/routes.rb
@@ -3,10 +3,3 @@ Rails.application.routes.draw do
     resources :reviews, only: [:new, :create]
   end
 end
-
-# restaurant_reviews POST /restaurants/:restaurant_id/reviews(.:format)                                            reviews#create
-#     new_restaurant_review GET  /restaurants/:restaurant_id/reviews/new(.:format)                                        reviews#new
-#               restaurants GET  /restaurants(.:format)                                                                   restaurants#index
-#                           POST /restaurants(.:format)                                                                   restaurants#create
-#            new_restaurant GET  /restaurants/new(.:format)                                                               restaurants#new
-#                restaurant GET  /restaurants/:id(.:format)                                                               restaurants#show
